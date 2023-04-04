79a663314345284b7ad1933363b5b7197cfc2ff8
DLC <shabrov23.02@gmail.com>
2023-04-05 00:50:56 +0300

    RuntimeMetrics

---
+++ internal/agent/metrics/RuntimeMetrics.go
@@ -0,0 +1,58 @@
+package metrics
+
+import "runtime"
+
+type MemMetrics struct {
+	gauge   map[string]float64
+	counter map[string]int64
+}
+
+func (metrics *MemMetrics) Init() {
+	metrics.gauge = make(map[string]float64)
+	metrics.counter = make(map[string]int64)
+	gaugeMetrics := []string{"Alloc", "BuckHashSys", "Frees", "GCCPUFraction", "GCSys", "HeapAlloc", "HeapIdle", "HeapInuse", "HeapObjects",
+		"HeapReleased", "HeapSys", "LastGC", "Lookups", "MCacheInuse", "MCacheSys", "MSpanInuse", "MSpanSys", "Mallocs",
+		"NextGC", "NumForcedGC", "NumGC", "OtherSys", "PauseTotalNs", "StackInuse", "StackSys", "Sys", "TotalAlloc"}
+	counterMetrics := []string{"PollCount"}
+	for _, metric := range gaugeMetrics {
+		metrics.gauge[metric] = 0
+	}
+	for _, metric := range counterMetrics {
+		metrics.counter[metric] = 0
+	}
+}
+func (metrics *MemMetrics) Check() {
+	var Runtime runtime.MemStats
+	runtime.ReadMemStats(&Runtime)
+	metrics.gauge["Alloc"] = float64(Runtime.Alloc)
+	metrics.gauge["BuckHashSys"] = float64(Runtime.BuckHashSys)
+	metrics.gauge["Frees"] = float64(Runtime.Frees)
+	metrics.gauge["GCCPUFraction"] = float64(Runtime.GCCPUFraction)
+	metrics.gauge["GCSys"] = float64(Runtime.GCSys)
+	metrics.gauge["HeapAlloc"] = float64(Runtime.HeapAlloc)
+	metrics.gauge["HeapIdle"] = float64(Runtime.HeapIdle)
+	metrics.gauge["HeapInuse"] = float64(Runtime.HeapInuse)
+	metrics.gauge["HeapObjects"] = float64(Runtime.HeapObjects)
+	metrics.gauge["HeapReleased"] = float64(Runtime.HeapReleased)
+	metrics.gauge["HeapSys"] = float64(Runtime.HeapSys)
+	metrics.gauge["LastGC"] = float64(Runtime.LastGC)
+	metrics.gauge["Lookups"] = float64(Runtime.Lookups)
+	metrics.gauge["MCacheInuse"] = float64(Runtime.MCacheInuse)
+	metrics.gauge["MCacheSys"] = float64(Runtime.MCacheSys)
+	metrics.gauge["MSpanInuse"] = float64(Runtime.MSpanInuse)
+	metrics.gauge["MSpanSys"] = float64(Runtime.MSpanSys)
+	metrics.gauge["Mallocs"] = float64(Runtime.Mallocs)
+	metrics.gauge["NextGC"] = float64(Runtime.NextGC)
+	metrics.gauge["NumForcedGC"] = float64(Runtime.NumForcedGC)
+	metrics.gauge["NumGC"] = float64(Runtime.NumGC)
+	metrics.gauge["OtherSys"] = float64(Runtime.OtherSys)
+	metrics.gauge["PauseTotalNs"] = float64(Runtime.PauseTotalNs)
+	metrics.gauge["StackInuse"] = float64(Runtime.StackInuse)
+	metrics.gauge["StackSys"] = float64(Runtime.StackSys)
+	metrics.gauge["Sys"] = float64(Runtime.Sys)
+	metrics.gauge["TotalAlloc"] = float64(Runtime.TotalAlloc)
+	metrics.counter["PollCount"]++
+}
+func GenerateUrl(host string) {
+
+}
