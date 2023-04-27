4c5d9cd07c94bb9fca9080f2eb07bb2ab2a87ccf
DLC <shabrov23.02@gmail.com>
2023-04-27 18:58:31 +0300

    CompressionResponse

--- internal/server/middleware/gzip/gzipper.go
+++ internal/server/middleware/gzip/gzipper.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 )
 
+type gzipWriter struct {
+	gin.ResponseWriter
+	writer *gzip.Writer
+}
+
 func DefaultDecompressHandle(gin *gin.Context) {
 	if gin.Request.Header.Get("Content-Encoding") != "gzip" {
 		gin.Next()
@@ -21,6 +26,21 @@ func DefaultDecompressHandle(gin *gin.Context) {
 		return
 	}
 	gin.Request.Body = r
+}
+func DefaultCompressionResponse(gin *gin.Context) {
+	if gin.Request.Header.Get("Accept-Encoding") != "gzip" {
+		gin.Next()
+		return
+	}
+	gz, err := gzip.NewWriterLevel(gin.Writer, gzip.BestSpeed)
+	defer gz.Close()
+	if err != nil {
+		gin.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
 
-	gin.Writer.Header().Add("Content-Encoding", "gzip")
+	gin.Header("Content-Encoding", "gzip")
+
+	gin.Writer = &gzipWriter{gin.Writer, gz}
+
 }
