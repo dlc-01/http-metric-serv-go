f64c5af25533455df206b1569454bde7bbc86a33
DLC <shabrov23.02@gmail.com>
2023-05-01 17:15:02 +0300

    fix?

--- internal/server/middleware/gzip/gzipper.go
+++ internal/server/middleware/gzip/gzipper.go
@@ -4,6 +4,7 @@ import (
 	"compress/gzip"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 type gzipWriter struct {
@@ -13,10 +14,15 @@ type gzipWriter struct {
 
 func DefaultDecompressHandle(gin *gin.Context) {
 	if gin.Request.Header.Get("Content-Encoding") == "gzip" {
-		if gin.Request.Body == nil {
-			gin.Next()
+		gz, err := gzip.NewWriterLevel(gin.Writer, gzip.BestSpeed)
+		if err != nil {
+			gin.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
+
+		gin.Writer = &gzipWriter{gin.Writer, gz}
+		gz.Close()
+
 		r, err := gzip.NewReader(gin.Request.Body)
 		if err != nil {
 			gin.AbortWithError(http.StatusBadRequest, err)
@@ -24,19 +30,16 @@ func DefaultDecompressHandle(gin *gin.Context) {
 		}
 		gin.Request.Body = r
 		r.Close()
-		//} else if !strings.Contains(gin.Request.Header.Get("Accept-Encoding"), "gzip") && !strings.Contains(gin.Request.Header.Get("Content-Encoding"), "gzip") {
-		//	gz, err := gzip.NewWriterLevel(gin.Writer, gzip.BestSpeed)
-		//
-		//	if err != nil {
-		//		gin.AbortWithError(http.StatusInternalServerError, err)
-		//		return
-		//	}
-		//
-		//	gin.Header("Content-Encoding", "gzip")
-		//
-		//	gin.Writer = &gzipWriter{gin.Writer, gz}
-		//	gz.Close()
-		//}
 
+	} else if strings.Contains(gin.Request.Header.Get("Accept-Encoding"), "gzip") && !strings.Contains(gin.Request.Header.Get("Content-Encoding"), "gzip") {
+		gz, err := gzip.NewWriterLevel(gin.Writer, gzip.BestSpeed)
+		if err != nil {
+			gin.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		gin.Header("Content-Encoding", "gzip")
+		gin.Writer = &gzipWriter{gin.Writer, gz}
+		gz.Close()
 	}
+
 }
