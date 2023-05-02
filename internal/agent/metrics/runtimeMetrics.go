e0bd5c2960748b8e742e80c2459d3e5fdf9fbeb3
DLC <shabrov23.02@gmail.com>
2023-05-02 21:45:58 +0300

    Update runtimeMetrics.go

--- internal/agent/metrics/runtimeMetrics.go
+++ internal/agent/metrics/runtimeMetrics.go
@@ -63,7 +63,7 @@ func (metrics *MemStorage) GenerateRequestBody(types string, metric string, i64 
 			Value: &f64,
 		})
 		if err != nil {
-			panic(fmt.Errorf("cannot marshal request to json: %w", err))
+			panic(fmt.Errorf("cannot marshal request to jsonh: %w", err))
 		}
 		var buf bytes.Buffer
 		gz := gzip.NewWriter(&buf)
@@ -77,7 +77,7 @@ func (metrics *MemStorage) GenerateRequestBody(types string, metric string, i64 
 			Delta: &i64,
 		})
 		if err != nil {
-			panic(fmt.Errorf("cannot marshal request to json: %w", err))
+			panic(fmt.Errorf("cannot marshal request to jsonh: %w", err))
 		}
 		var buf bytes.Buffer
 		gz := gzip.NewWriter(&buf)
