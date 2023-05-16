a42cf458e3360424bc258c9b0cb148076751ed1b
DLC <shabrov23.02@gmail.com>
2023-05-16 17:54:34 +0300

    added test iter12

--- internal/server/handlers/butchjson/updatesButchJSONHandler_test.go
+++ internal/server/handlers/butchjson/updatesButchJSONHandler_test.go
@@ -17,21 +17,37 @@ func TestUpdatesButchJSONHandler(t *testing.T) {
 	router := gin.Default()
 	router.Use(gzip.Gzip(gzip.BestCompression))
 	router.POST("/updates/", UpdatesButchJSONHandler)
-	storage.Init()
 
 	testValue := 2022.02
+	testValues := 2022.01
 	testDelta := int64(24)
+	testDeltas := int64(48)
 
 	tests := []struct {
 		name         string
 		url          string
 		expectedCode int
+		responseBody []metrics.Metric
 		expectedBody []metrics.Metric
 	}{
 		{
 			name:         `true gauge post`,
 			expectedCode: http.StatusOK,
 			url:          `/updates/`,
+			responseBody: []metrics.Metric{
+				{
+					ID:    "TestCounter",
+					MType: metrics.CounterType,
+					Delta: &testDelta,
+					Value: nil,
+				},
+				{
+					ID:    "TestGauge",
+					MType: metrics.GaugeType,
+					Delta: nil,
+					Value: &testValue,
+				},
+			},
 			expectedBody: []metrics.Metric{
 				{
 					ID:    "TestCounter",
@@ -48,10 +64,55 @@ func TestUpdatesButchJSONHandler(t *testing.T) {
 			},
 		},
 		{
+			name:         `true gauge post`,
+			expectedCode: http.StatusOK,
+			url:          `/updates/`,
+			responseBody: []metrics.Metric{
+				{
+					ID:    "TestCounter",
+					MType: metrics.CounterType,
+					Delta: &testDelta,
+					Value: nil,
+				},
+				{
+					ID:    "TestGauge",
+					MType: metrics.GaugeType,
+					Delta: nil,
+					Value: &testValues,
+				},
+				{
+					ID:    "TestCounter",
+					MType: metrics.CounterType,
+					Delta: &testDelta,
+					Value: nil,
+				},
+				{
+					ID:    "TestGauge",
+					MType: metrics.GaugeType,
+					Delta: nil,
+					Value: &testValue,
+				},
+			},
+			expectedBody: []metrics.Metric{
+				{
+					ID:    "TestCounter",
+					MType: metrics.CounterType,
+					Delta: &(testDeltas),
+					Value: nil,
+				},
+				{
+					ID:    "TestGauge",
+					MType: metrics.GaugeType,
+					Delta: nil,
+					Value: &testValue,
+				},
+			},
+		},
+		{
 			name:         `wrong metric`,
 			expectedCode: http.StatusNotImplemented,
 			url:          `/updates/`,
-			expectedBody: []metrics.Metric{
+			responseBody: []metrics.Metric{
 				{
 					ID:    "TestWrongMetric",
 					MType: "qwert",
@@ -63,27 +124,30 @@ func TestUpdatesButchJSONHandler(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		jsons, err := metrics.ToJSONMetrics(tt.expectedBody)
-		if err != nil {
-			logging.Fatalf("cannot generate request body: %w", err)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			storage.Init()
+			jsons, err := metrics.ToJSONMetrics(tt.responseBody)
+			if err != nil {
+				logging.Fatalf("cannot generate request body: %w", err)
+			}
 
-		req, err := http.NewRequest(http.MethodPost, tt.url, jsons)
-		if err != nil {
-			t.Fatal(err)
-		}
+			req, err := http.NewRequest(http.MethodPost, tt.url, jsons)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Content-Encoding", "gzip")
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Content-Encoding", "gzip")
 
-		w := httptest.NewRecorder()
-		router.ServeHTTP(w, req)
+			w := httptest.NewRecorder()
+			router.ServeHTTP(w, req)
 
-		assert.Equal(t, tt.expectedCode, w.Code)
+			assert.Equal(t, tt.expectedCode, w.Code)
 
-		if tt.expectedCode == 200 {
-			data := storage.GetMetrics()
-			assert.Equal(t, tt.expectedBody, data)
-		}
+			if tt.expectedCode == 200 {
+				data := storage.GetMetrics()
+				assert.Equal(t, tt.expectedBody, data)
+			}
+		})
 	}
 }
