577c02b044310d0b1134ee18034db7702597a138
DLC <shabrov23.02@gmail.com>
2023-06-03 14:33:58 +0300

    fix test for limit

--- internal/server/middleware/limit/limit_test.go
+++ internal/server/middleware/limit/limit_test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http/httptest"
-	"reflect"
 	"sync"
 	"testing"
 	"time"
@@ -12,67 +11,66 @@ import (
 
 func TestLimit(t *testing.T) {
 	type args struct {
-		limit int
+		limit    int
+		attempts int
+		goodTest bool
 	}
 	tests := []struct {
 		name string
 		args args
-		want gin.HandlerFunc
 	}{
-		// TODO: Add test cases.
+		{name: "testOneLimit",
+			args: args{
+				limit:    1,
+				attempts: 1,
+				goodTest: true,
+			},
+		},
+		{name: "testMoreLimit",
+			args: args{
+				limit:    5,
+				attempts: 100,
+				goodTest: false,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Limit(tt.args.limit); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Limit() = %v, want %v", got, tt.want)
+			var failed int
+			var wg sync.WaitGroup
+
+			gin.SetMode(gin.TestMode)
+			router := gin.New()
+			router.Use(Limit(tt.args.limit))
+			router.GET("/", func(*gin.Context) {
+				time.Sleep(5 * time.Microsecond)
+			})
+			if tt.args.goodTest {
+				w := performRequest("GET", "/", router)
+				assert.Equal(t, 200, w.Code)
+			} else {
+				for i := 0; i < tt.args.attempts; i++ {
+					wg.Add(1)
+					go func() {
+						defer wg.Done()
+						w := performRequest("GET", "/", router)
+						if w.Code == 502 {
+							failed++
+						}
+					}()
+				}
+				wg.Wait()
+
+				assert.True(t, failed > 0)
 			}
+
 		})
 	}
 }
-func TestLimits(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-	router := gin.New()
-	router.Use(Limit(1))
-	router.GET("/", func(*gin.Context) {
-		time.Sleep(500 * time.Microsecond)
-	})
 
-	w := performRequest("GET", "/", router)
-	assert.Equal(t, 200, w.Code)
-
-}
 func performRequest(method, target string, router *gin.Engine) *httptest.ResponseRecorder {
 	r := httptest.NewRequest(method, target, nil)
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, r)
 	return w
-}
-
-func TestFulled(t *testing.T) {
-	const max = 5
-
-	attempts := 1000
-	var failed int
-	var wg sync.WaitGroup
-
-	gin.SetMode(gin.TestMode)
-	router := gin.New()
-	router.Use(Limit(max))
-	router.GET("/", func(*gin.Context) {
-		time.Sleep(5 * time.Microsecond)
-	})
-
-	for i := 0; i < attempts; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			w := performRequest("GET", "/", router)
-			if w.Code == 502 {
-				failed++
-			}
-		}()
-	}
-	wg.Wait()
-
-	assert.True(t, failed > 0)
 }
