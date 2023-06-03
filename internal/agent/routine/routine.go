5089604ba2107b5e1b187e98baa5f04ffc794874
DLC <shabrov23.02@gmail.com>
2023-06-03 16:04:07 +0300

    ?

--- internal/agent/routine/routine.go
+++ internal/agent/routine/routine.go
@@ -19,8 +19,8 @@ func Run(cfg *config.AgentConfig) {
 	//TODO тз на эти итеры было оч расплывчатое мб что то не правильно понял, тесты тоже не работают
 	reportTicker := time.Duration(time.Second * time.Duration(cfg.Report))
 	poolTicker := time.Duration(time.Second * time.Duration(cfg.Poll))
-	chanStor := make(chan []metrics.Metric)
+	chanStor := make(chan []metrics.Metric, cfg.LimitM)
 	go collector.CollectMetrics(context.Background(), chanStor, poolTicker)
-	go sendMetrics(cfg, <-chanStor, reportTicker)
+	go sendMetrics(cfg, chanStor, reportTicker)
 
 }
